8ab9b674a27c6e3f9010c80854435c76f5f60527
Sakai-san <o_edo@hotmail.com>
2019-06-04 08:50:38 +0200

    Skeleton code session.

---
+++ index.js
@@ -0,0 +1,64 @@
+const employees = [
+  {
+    id: 1,
+    fist: "Michelle",
+    last: "Vez",
+    gender: "f",
+    bossId: 3,
+    salary: 50000
+  },
+  {
+    id: 2,
+    fist: "Big",
+    last: "Boss",
+    gender: "f",
+    bossId: 2,
+    salary: 200000
+  },
+  {
+    id: 3,
+    fist: "Sub",
+    last: "Boss",
+    gender: "m",
+    bossId: 2,
+    salary: 140000
+  },
+  {
+    id: 5,
+    fist: "Simon",
+    last: "Littrel",
+    gender: "m",
+    bossId: 3,
+    salary: 140000
+  }
+];
+
+/**
+ * Assignment 1
+ * isEmployeeMan : Employee => Boolean
+ */
+
+const isEmployeeMan = ({ gender }) => gender === "m";
+
+/**
+ * Assignment 2
+ * employeesWorkingFor : number [Employees] => [Employees]
+ */
+
+const employeesWorkingFor = (bossId, employees) =>
+  employees.filter(
+    employee => employee.bossId === bossId && employee.id !== employee.bossId
+  );
+
+console.log(employeesWorkingFor(2, employees));
+
+/**
+ * Assignment 3
+ * salarySumByGender : predicate [Employees] => number
+ */
+const not = f => a => !f(a);
+
+const salarySumByGender = (isMan, employees) =>
+  employees.reduce((acc, item) => (isMan(item) ? acc : acc + item.salary), 0);
+
+console.log(salarySumByGender(not(isEmployeeMan), employees));
